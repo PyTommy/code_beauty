11c2b4957f2f7d7b7bb761dc16e57e9fb0018b25
Hiroki Tominaga <hiroki.engineer@gmail.com>
2021-11-06 02:26:27 +0900

    added components

---
+++ src/components/atoms/TextInput/TextIput.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+interface Props {}
+
+export const TextIput = (props: Props) => {
+  return <div></div>
+}
---
+++ src/components/molecules/FormTextInput/FormTextInput.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+interface Props {}
+
+export const FormTextInput = (props: Props) => {
+  return <div></div>
+}
