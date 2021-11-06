8f7baaadb4c8bf041662f615e2d6f0d9cc551b6e
Hiroki Tominaga <hiroki.engineer@gmail.com>
2021-11-07 02:14:00 +0900

    不必要なコード削除

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import {} from './components/atoms/Button'
 
 ReactDOM.render(<h1>Hello React!</h1>, document.getElementById('root'))
