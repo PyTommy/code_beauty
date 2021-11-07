4f726c9c62c6d3193c92f03a7d0f3e6738752b1c
Hiroki Tominaga <hiroki.engineer@gmail.com>
2021-11-07 22:04:22 +0900

    Fixed bag

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { AppProvider } from './utils/AppProvider'
+import { AppProvider } from './components/organisms/common/AppProvider'
 
 export const App = () => {
   return <AppProvider>Hello World!</AppProvider>
