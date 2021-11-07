d0b7d80295029ffd8fa4e537880c182b8649fe34
Hiroki Tominaga <hiroki.engineer@gmail.com>
2021-11-07 21:00:03 +0900

    moved AppProvider to components folder

--- src/components/organisms/common/AppProvider/AppProvider.tsx
+++ src/components/organisms/common/AppProvider/AppProvider.tsx
@@ -2,8 +2,8 @@ import { ThemeProvider as MuiThemeProvider } from '@mui/material'
 import React from 'react'
 import { ThemeProvider as StyledThemeProvider } from 'styled-components'
 
-import { muiTheme } from './styles/muiTheme'
-import { styledTheme } from './styles/styledTheme'
+import { muiTheme } from '@/utils/styles/muiTheme'
+import { styledTheme } from '@/utils/styles/styledTheme'
 
 export const AppProvider: React.FC = ({ children }) => (
   <StyledThemeProvider theme={styledTheme}>
